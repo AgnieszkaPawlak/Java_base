9870c0c2eb0cd4b19d19c892d852031fa3a7f1d0
Agnieszka Pawlak <agnieszka.zd@interia.pl>
2023-05-26 15:18:21 +0200

    Added output list exercise.

---
+++ src/main/java/exercise1/outputlist/OutputListExercise.java
@@ -0,0 +1,34 @@
+package exercise1.outputlist;
+
+public class OutputListExercise {
+    public static void main(String[] args) {
+        //Exercise 1
+        /*Print as:
+         TO-DO:
+         - Monday
+            -> Go shopping
+            -> Clean the house
+         - Tuesday
+            -> L earn Java
+         */
+        /*System.out.println("TO-DO:");
+        System.out.println("\t- Monday");
+        System.out.print("\t\t -> Go shopping");
+        System.out.print("\n\t\t -> Clean the house");
+        System.out.print("\n\t - Tuesday");
+        System.out.print("\n\t\t -> L \bearn Java");*/
+
+        //Exercise 2
+        /*Convert above to one line
+          System.out.print("TO-DO\n\t - Monday\n\t\t ->Go shopping\n\t\t -> Clean the house \n\t - Tuesday\n\t\t -> L \bearn Java");*/
+
+        //Exercise 3
+        /*Write a tab in odd lines*/
+        System.out.print("The\n");
+        System.out.println("\tbest");
+        System.out.println("day");
+        System.out.println("\tin");
+        System.out.println("my");
+        System.out.println("live.");
+    }
+}
